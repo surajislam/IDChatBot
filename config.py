f08d9a7436ea8366ddb7227ddc8e9cf68dfd0a71
surajislam <154444093+surajislam@users.noreply.github.com>
2024-09-21 11:05:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,18 +14,18 @@ API_HASH = getenv("API_HASH", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
 
 ### ❖ ➥
-OWNER_ID = int(getenv("OWNER_ID", "7178932244"))
+OWNER_ID = int(getenv("OWNER_ID", "6903379369"))
 
 ### ❖ ➥
 MONGO_URL = getenv("MONGO_URL", None)
 
 ### ❖ ➥
-SUPPORT_GRP = getenv("SUPPORT_GRP", "pikachuworld0099)
+SUPPORT_GRP = getenv("SUPPORT_GRP", "+BPN5Edy5g6Y5NTll)
 
 ### ❖ ➥
-UPDATE_CHNL = getenv("UPDATE_CHNL", "anokhikeduniya)
+UPDATE_CHNL = getenv("UPDATE_CHNL", "BPN5Edy5g6Y5NTll)
 
 ### ❖ ➥
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_SB_ANGLE_ll)
+OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_SB_SIMPLE_ll)
 
 
